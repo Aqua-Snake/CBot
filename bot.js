c1af065c391325ffb7c14b5a909a54ba84ff0fde
Aqua Snake <87765985+Aqua-Snake@users.noreply.github.com>
2021-09-01 20:06:51 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -84,7 +84,7 @@ async function whatsAsena () {
     setInterval(async () => { 
         var getGMTh = new Date().getHours()
         var getGMTm = new Date().getMinutes()
-        await axios.get('https://gist.githubusercontent.com/phaticusthiccy/d0d1855bd0098d773759b4f3345bd292/raw/').then(async (ann) => {
+        await axios.get('https://gist.githubusercontent.com/Aqua-Snake/ff44c663f4435d3fc998dbb98615214a/raw/').then(async (ann) => {
             const { infotr, infoen, infoes, infopt, infoid, infoaz, infohi, infoml, inforu} = ann.data.announcements          
             if (infotr !== '' && config.LANG == 'TR') {
                 while (getGMTh == 19 && getGMTm == 1) { 
