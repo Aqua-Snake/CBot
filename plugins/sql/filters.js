9b73d658c88a113ea90e4862eae6109760f35e71
Aqua Snake <87765985+Aqua-Snake@users.noreply.github.com>
2021-09-07 22:30:31 +0530

    ☞ Cyber Army Bot ࿊ 2.0v
    Update 2.0v

--- plugins/sql/filters.js
+++ plugins/sql/filters.js
@@ -1,9 +1,9 @@
-/* Copyright (C) 2020 Yusuf Usta.
+/* Copyright (C) 2021 Aqua Snake.
 
 Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.
 
-WhatsAsena - Yusuf Usta
+Cyber Army Bot  - Aqua-Snake
 */
 
 const config = require('../../config');
