89eed25a14046281c74cc2aa0c200244926eca59
Aqua Eagle <91973695+Aqua-Eagle@users.noreply.github.com>
2021-10-09 08:53:38 -0700

    Update helpers.js

--- helpers.js
+++ helpers.js
@@ -1,5 +1,5 @@
 /*
-# Copyright (C) 2020 MuhammedKpln.
+# Copyright (C) 2020 Aqua-Snake.
 #
 # Cyber Bot is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
